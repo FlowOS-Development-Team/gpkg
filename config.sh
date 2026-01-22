7ce7618356572f15a92f4b5179c49752456ed75d
Elias Stinson <favorite@favoriteone.dev>
2026-01-22 12:06:26 -0800

    added rudimentary git repo pack add command

--- config.sh
+++ config.sh
@@ -1,3 +1,7 @@
 warning() {
   echo "WARNING: $*" 1>&2
 }
+source=/etc/update.d/
+pkgloc=/usr/pkg/
+pkgtext=".group"
+installfile="install.sh"
---
+++ gpadd.sh
@@ -0,0 +1,15 @@
+source "./config.sh"
+
+if [ -z "$1" ]; then
+ echo "gpadd: gitpkg group requires a name" >&2
+elif [ -z "$2" ]; then
+ echo "gpadd: gitpkg group requires a contribiutor url" >&2
+else
+ echo "Adding package group to $source, this should only take a second....."
+ cd "$source"
+ mkdir "$1"
+ echo "Folder created for new package group....."
+ cd "$1"
+ echo "Creating source file....."
+ echo $2 > "$1.source"
+fi
