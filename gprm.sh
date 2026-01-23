64a8b5d7cf13909c7009ac9b5b55eada0badd13a
EJ Stinson <93455158+FavroiteGamers@users.noreply.github.com>
2026-01-23 14:28:38 -0800

    added arg handling

--- gprm.sh
+++ gprm.sh
@@ -9,14 +9,19 @@ info() {
  } # define usage function
 #helptext to be updated in a later beta version when there is a uni bin 
 # Load flags
-while getopt "f" flag; do
+if [[ $# -eq 0 ]]; then
+    arg=false
+    while getopt "f" flag; do
     case "$flag" in
         f) FORCE="true" ;;
         ?) info ;;
         *) info
            exit 1 ;;
-    esac
-done
+     esac
+    done
+elif [[ $# -eq 1]]; then
+    arg=true
+else
 if [ -z "$1" ]; then
  echo "gprm: gitpkg remover requires a package group to delete from" >&2
  helpc
@@ -35,4 +40,5 @@ else
         echo "Removing package $pkg from group $group..."
         remove "-f" "$pkg"
     done
+fi
 fi
