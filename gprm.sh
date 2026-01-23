65452b1fa2037ba94affbca2bb83881c92876f00
EJ Stinson <93455158+FavroiteGamers@users.noreply.github.com>
2026-01-23 11:23:04 -0800

    added zero var handling from gpadd into gprm

--- gprm.sh
+++ gprm.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 source /etc/gpkg/env.sh
+if [ -z "$1" ]; then
+ echo "gprm: gitpkg remover requires a name" >&2
+elif [ -z "$2" ]; then
+ echo "gprm: gitpkg remover requires a pack-group" >&2
+else
