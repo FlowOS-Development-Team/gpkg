3c06084ccba9d01217c2618faa74ee4edbe0e731
EJ Stinson <93455158+FavroiteGamers@users.noreply.github.com>
2026-01-23 14:02:48 -0800

    fixed conflict with regular help and our help; changed help to info

--- gprm.sh
+++ gprm.sh
@@ -4,24 +4,24 @@
 # Initialize environment
 source /etc/gpkg/env.sh
 is_root
-help { usage "gprm" "-f" "package-group" "package1" >&2 } // define usage function
+info { usage "gprm" "-f" "package-group" "package1" >&2 } // define usage function
 #helpext to be updated in a later beta version when there is a uni bin 
 # Load flags
 while getops "f" flag; do
     case "$flag" in
         f) FORCE="true" ;;
-        ?) help;
+        ?) info;
         *) 
            exit 1 ;;
     esac
 done
 if [ -z "$1" ]; then
  echo "gprm: gitpkg remover requires a package group to delete from" >&2
- help
+ helpc
  quit; 
 elif [ -z "$2" ]; then
  echo "gprm: gitpkg remover requires (a) package(s) to remove file from" >&2
- help
+ info
  quit;
 else
     group="$1"
